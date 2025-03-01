a1aa27233926185467d30ade197a3c3def2b180d
Markus Oliver Annuk <markus.annuk@pipedrive.com>
2025-03-01 20:32:36 +0200

    build error fixes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,98 +1,102 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
-import { useTranslation } from 'react-i18next';
-import { motion } from 'framer-motion'
-import { Button } from "@/components/ui/button"
-import { useFadeIn } from '@/hooks/useFadeIn'
-import { UpcomingTournaments } from './-components/comingTournaments';
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { useFadeIn } from "@/hooks/useFadeIn";
+import { UpcomingTournaments } from "./-components/comingTournaments";
 
-import { TopMonthPerformers } from './-components/topPerformers';
-import { LatestArticles } from './-components/latestArticles';
-import { UseGetArticles } from '@/queries/articles';
-import { TopFive } from './-components/topFive';
+import { TopMonthPerformers } from "./-components/topPerformers";
+import { TopFive } from "./-components/topFive";
 
-
-export const Route = createFileRoute('/')({
-    component: Index,
-    // loader: async ({ context: { queryClient } }) => {
-    //     const articledata = queryClient.ensureQueryData(UseGetArticles())
-    //     return articledata
-    // }
-})
+export const Route = createFileRoute("/")({
+  component: Index,
+  // loader: async ({ context: { queryClient } }) => {
+  //     const articledata = queryClient.ensureQueryData(UseGetArticles())
+  //     return articledata
+  // }
+});
 
 function Index() {
+  const { t } = useTranslation();
 
-    const articledata = Route.useLoaderData()
+  const [heroControls, heroRef] = useFadeIn();
+  const [tournamentsControls, tournamentsRef] = useFadeIn(0.2);
 
-    const { t } = useTranslation();
-
-    const [heroControls, heroRef] = useFadeIn()
-    const [tournamentsControls, tournamentsRef] = useFadeIn(0.2)
-
-    return (
-        <div className="bg-gradient-to-b from-white to-gray-200">
-            <div className="relative h-[30vh] bg-cover bg-center" style={{ backgroundImage: "url('/test/table_tennis_background.png?height=1080&width=1920')" }}>
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                    <motion.div
-                        className="text-center text-white"
-                        ref={heroRef}
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={heroControls}
-                    >
-                        <h1 className="text-5xl font-bold mb-4">{t('homepage.title')}</h1>
-                        <p className="text-xl mb-8">{t('homepage.description')}</p>
-                        <Link to="/voistlused">
-                            <Button size="lg" className="bg-secondary hover:bg-blue-700 text-white">{t('homepage.title_button')}</Button>
-                        </Link>
-                    </motion.div>
-                </div>
-            </div>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-                <motion.section
-                    className="mb-16"
-                    ref={tournamentsRef}
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={tournamentsControls}
-                >
-                    <UpcomingTournaments />
-                </motion.section>
-
-
-                <motion.div
-                    initial={{ opacity: 0, x: 50 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5, delay: 0.6 }}
-                    className="mb-16"
-                >
-                    {/* <LatestArticles articles={articledata.data.slice(0, 3)} /> */}
-                </motion.div>
-
-                <div className="flex justify-center sm:flex-row flex-col gap-10 w-full mb-8">
-                    <motion.div
-                        initial={{ opacity: 0, x: -50 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.5, delay: 1 }}
-                        className="flex-1"
-                    >
-                        <TopFive gender="men" />
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, x: -50 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.5, delay: 1 }}
-                        className="flex-1"
-                    >
-                        <TopFive gender="women" />
-                    </motion.div>
-                </div>
-                <motion.div
-                    initial={{ opacity: 0, x: -50 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5, delay: 1 }}
-                    className=""
-                >
-                    <TopMonthPerformers />
-                </motion.div>
-            </div>
+  return (
+    <div className="bg-gradient-to-b from-white to-gray-200">
+      <div
+        className="relative h-[30vh] bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url('/test/table_tennis_background.png?height=1080&width=1920')",
+        }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <motion.div
+            className="text-center text-white"
+            ref={heroRef}
+            initial={{ opacity: 0, y: 20 }}
+            animate={heroControls}
+          >
+            <h1 className="text-5xl font-bold mb-4">{t("homepage.title")}</h1>
+            <p className="text-xl mb-8">{t("homepage.description")}</p>
+            <Link to="/voistlused">
+              <Button
+                size="lg"
+                className="bg-secondary hover:bg-blue-700 text-white"
+              >
+                {t("homepage.title_button")}
+              </Button>
+            </Link>
+          </motion.div>
         </div>
-    )
+      </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <motion.section
+          className="mb-16"
+          ref={tournamentsRef}
+          initial={{ opacity: 0, y: 20 }}
+          animate={tournamentsControls}
+        >
+          <UpcomingTournaments />
+        </motion.section>
+
+        <motion.div
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+          className="mb-16"
+        >
+          {/* <LatestArticles articles={articledata.data.slice(0, 3)} /> */}
+        </motion.div>
+
+        <div className="flex justify-center sm:flex-row flex-col gap-10 w-full mb-8">
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 1 }}
+            className="flex-1"
+          >
+            <TopFive gender="men" />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 1 }}
+            className="flex-1"
+          >
+            <TopFive gender="women" />
+          </motion.div>
+        </div>
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+          className=""
+        >
+          <TopMonthPerformers />
+        </motion.div>
+      </div>
+    </div>
+  );
 }
--- src/routes/voistlused/$tournamentid/-components/navbar.tsx
+++ src/routes/voistlused/$tournamentid/-components/navbar.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import { Link, useParams, useLocation } from "@tanstack/react-router"
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { cn, formatDate, formatDateRange } from "@/lib/utils"
-import { useTournament } from "./tournament-provider"
+import { Link, useParams, useLocation } from "@tanstack/react-router";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn, formatDateRange } from "@/lib/utils";
+import { useTournament } from "./tournament-provider";
 
 const NavLinks = [
   { name: "Info", href: "/" },
@@ -14,21 +14,23 @@ const NavLinks = [
   { name: "Juhend", href: "/juhend" },
   { name: "Sponsorid", href: "/sponsorid" },
   { name: "Meedia", href: "/meedia" },
-]
+];
 
 const Navbar = () => {
-  const params = useParams({ strict: false })
-  const location = useLocation()
+  const params = useParams({ strict: false });
+  const location = useLocation();
 
-  const currentTab = location.pathname.split("/").pop() || "/"
+  const currentTab = location.pathname.split("/").pop() || "/";
 
-  const tournament = useTournament()
+  const tournament = useTournament();
 
   return (
     <div className="">
       <div className="pt-12 pb-4 text-[#363636] bg-[#FBFCFD]">
         <div className="container mx-auto px-4">
-          <h1 className="text-4xl text-center font-semibold mb-4">{tournament.name}</h1>
+          <h1 className="text-4xl text-center font-semibold mb-4">
+            {tournament.name}
+          </h1>
           <p className="text-xl text-center">{`${formatDateRange(new Date(tournament.start_date), new Date(tournament.end_date))} • ${tournament.location}`}</p>
         </div>
       </div>
@@ -36,13 +38,19 @@ const Navbar = () => {
         <Tabs value={currentTab} className="w-full flex justify-center">
           <TabsList className="flex-wrap w-[80%] mx-auto">
             {NavLinks.map((link) => (
-              <Link className="" to={`/voistlused/${params.tournamentid}${link.href}`} key={link.name}>
+              <Link
+                className=""
+                to={`/voistlused/${params.tournamentid}${link.href}`}
+                key={link.name}
+              >
                 <TabsTrigger
                   value={link.href.replace("/", "")}
                   className={cn(
                     "text-sm sm:text-base px-3 py-2 w-auto lg:-[100px] xl:w-[125px] 2xl:w-[150px]",
-                    currentTab === link.href.replace("/", "") && "bg-[#3C83F6] text-white",
-                    currentTab !== link.href.replace("/", "") && "hover:bg-[#3C83F6]/10",
+                    currentTab === link.href.replace("/", "") &&
+                      "bg-[#3C83F6] text-white",
+                    currentTab !== link.href.replace("/", "") &&
+                      "hover:bg-[#3C83F6]/10",
                   )}
                 >
                   {link.name}
@@ -53,8 +61,7 @@ const Navbar = () => {
         </Tabs>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
