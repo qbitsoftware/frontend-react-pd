1a81270935682bae1c9761763d8f7cc44ed87eca
hmahar <harrimahar@gmail.com>
2025-03-25 17:50:19 +0200

    removed i18 debug

--- src/i18n.ts
+++ src/i18n.ts
@@ -16,7 +16,7 @@ i18n
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         fallbackLng: 'et',
-        debug: true,
+        debug: false,
         // resources: {
         //     en: {
         //         translation: {
