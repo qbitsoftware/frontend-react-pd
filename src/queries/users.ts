b122bfeaca6eb9551530970e473d5b76c6fead0c
Markus Oliver Annuk <markus.annuk@pipedrive.com>
2025-04-27 22:57:41 +0300

    route path fix

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -1,179 +1,195 @@
-import { queryOptions, useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { LoginFormData } from "@/routes/login"
-import { axiosInstance } from "./axiosconf"
-import { User, UserLogin } from "@/types/users"
-import { RegisterFormData } from "@/routes/register"
+import {
+  queryOptions,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { LoginFormData } from "@/routes/login";
+import { axiosInstance } from "./axiosconf";
+import { User, UserLogin } from "@/types/users";
+import { RegisterFormData } from "@/routes/register";
 
 export interface LoginResponse {
-    data: UserLogin
-    message: string
-    error: string | null
+  data: UserLogin;
+  message: string;
+  error: string | null;
 }
-
 
 export interface UsersResponse {
-    data: User[]
-    message: string
-    error: string | null
+  data: User[];
+  message: string;
+  error: string | null;
 }
-
 
 export const useGetUser = () => {
-    return queryOptions<LoginResponse>({
-        queryKey: ["user"],
-        queryFn: async () => {
-            const { data } = await axiosInstance.get("/auth/check", {
-                withCredentials: true
-            })
-            return data
-        },
-        retry: false
-    })
-}
+  return queryOptions<LoginResponse>({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get("/auth/check", {
+        withCredentials: true,
+      });
+      return data;
+    },
+    retry: false,
+  });
+};
 
 export const useLogin = () => {
-    const queryClient = useQueryClient()
-    return useMutation({
-        mutationFn: async (formData: LoginFormData) => {
-            const { data } = await axiosInstance.post("/auth/login", formData, {
-                withCredentials: true
-            })
-            return data
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["user"] })
-        }
-    })
-}
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (formData: LoginFormData) => {
+      const { data } = await axiosInstance.post("/auth/login", formData, {
+        withCredentials: true,
+      });
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+  });
+};
 
 export const useGetCurrentUserQuery = () => {
-    return useQuery<LoginResponse>({
-        queryKey: ["user"],
-        queryFn: async () => {
-            const { data } = await axiosInstance.get(`/auth/check`, {
-                withCredentials: true
-            })
-            return data
-        },
-        staleTime: Infinity,
-        retry: false,
-        refetchOnWindowFocus: true,
-    })
-}
+  return useQuery<LoginResponse>({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get(`/auth/check`, {
+        withCredentials: true,
+      });
+      return data;
+    },
+    staleTime: Infinity,
+    retry: false,
+    refetchOnWindowFocus: true,
+  });
+};
 
 export const UseGetCurrentUser = () => {
-    return queryOptions<LoginResponse>({
-        queryKey: ["auth"],
-        queryFn: async () => {
-            const { data } = await axiosInstance.get(`/auth/check`, {
-                withCredentials: true
-            })
-            return data
-        },
-        refetchOnWindowFocus: true,
-    })
-}
-
+  return queryOptions<LoginResponse>({
+    queryKey: ["auth"],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get(`/auth/check`, {
+        withCredentials: true,
+      });
+      return data;
+    },
+    refetchOnWindowFocus: true,
+  });
+};
 
 export const useLogout = () => {
-    const queryClient = useQueryClient()
-    return useMutation({
-        mutationFn: async () => {
-            const { data } = await axiosInstance.post(`/auth/logout`, {}, {
-                withCredentials: true
-            })
-            return data
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async () => {
+      const { data } = await axiosInstance.post(
+        `/auth/logout`,
+        {},
+        {
+          withCredentials: true,
         },
-        onSuccess: () => {
-            queryClient.setQueryData(["user"], { data: null, message: "", error: null })
-            queryClient.invalidateQueries({ queryKey: ["user"] })
-            queryClient.refetchQueries({ queryKey: ["user"] })
-        }
-    })
-}
-
+      );
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.setQueryData(["user"], {
+        data: null,
+        message: "",
+        error: null,
+      });
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.refetchQueries({ queryKey: ["user"] });
+    },
+  });
+};
 
 export const UseGetUsersDebounce = (searchTerm: string) => {
-    return useQuery<UsersResponse>({
-        queryKey: ["users", searchTerm],
-        queryFn: async () => {
-            const { data } = await axiosInstance.get(`/api/v1/users?search=${searchTerm}`, {
-                withCredentials: true
-            })
-            return data
+  return useQuery<UsersResponse>({
+    queryKey: ["users", searchTerm],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get(
+        `/api/v1/users?search=${searchTerm}`,
+        {
+          withCredentials: true,
         },
-        enabled: !!searchTerm,
-    })
-}
+      );
+      return data;
+    },
+    enabled: !!searchTerm,
+  });
+};
 
 export const UseGetUsers = (searchTerm?: string) => {
-    if (searchTerm === undefined) {
-        searchTerm = ""
-    }
-    return queryOptions<UsersResponse>({
-        queryKey: ["users", searchTerm],
-        queryFn: async () => {
-            const { data } = await axiosInstance.get(`/api/v1/users?search=${searchTerm}`, {
-                withCredentials: true
-            })
-            return data
+  if (searchTerm === undefined) {
+    searchTerm = "";
+  }
+  return queryOptions<UsersResponse>({
+    queryKey: ["users", searchTerm],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get(
+        `/api/v1/users?search=${searchTerm}`,
+        {
+          withCredentials: true,
         },
-        enabled: !!searchTerm,
-    })
-}
+      );
+      return data;
+    },
+    enabled: !!searchTerm,
+  });
+};
 
-export const fetchUserByName = async (name: string): Promise<User| null> => {
-    try {
-        const { data } = await axiosInstance.get(`/api/v1/users?search=${name}`, {
-            withCredentials: true
-        })
-        return data.data[0] || null
-    } catch (error) {
-        console.error("Error fetching user by name:", error)
-        return null
-    }
-}
+export const fetchUserByName = async (name: string): Promise<User | null> => {
+  try {
+    const { data } = await axiosInstance.get(`/api/v1/users?search=${name}`, {
+      withCredentials: true,
+    });
+    return data.data[0] || null;
+  } catch (error) {
+    console.error("Error fetching user by name:", error);
+    return null;
+  }
+};
 
 export const useCreateUser = () => {
-    return useMutation({
-        mutationFn: async (formData: RegisterFormData) => {
-            const { data } = await axiosInstance.post("/auth/register", formData, {
-                withCredentials: true
-            })
-            return data
-        },
-    })
-}
+  return useMutation({
+    mutationFn: async (formData: RegisterFormData) => {
+      const { data } = await axiosInstance.post("/auth/register", formData, {
+        withCredentials: true,
+      });
+      return data;
+    },
+  });
+};
 
 export function useUsersCount() {
-    const { data, isLoading } = useQuery({
-      queryKey: ['users'],
-      queryFn: async () => {
-        const response = await axiosInstance.get('/api/v1/users', {
-          withCredentials: true
-        })
-        return response.data
-      }
-    })
-    
-    return {
-      count: data?.data?.length || 0,
-      isLoading
-    }
-  }
+  const { data, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const response = await axiosInstance.get("/api/v1/users", {
+        withCredentials: true,
+      });
+      return response.data;
+    },
+  });
+
+  return {
+    count: data?.data?.length || 0,
+    isLoading,
+  };
+}
 
 export interface FeedbackForm {
-    name: string;
-    title: string;
-    body: string;
+  name: string;
+  title: string;
+  body: string;
 }
 
 export const sendUserFeedback = async (feedback: FeedbackForm) => {
-    try {
-        await axiosInstance.post("/send-feedback", feedback)
-        return { success: true }
-    } catch (error) {
-        console.error("Failed to send feedback:", error)
-        return { success: false, error }
-    }
-}
+  try {
+    await axiosInstance.post("/api/v1/feedback", feedback);
+    return { success: true };
+  } catch (error) {
+    console.error("Failed to send feedback:", error);
+    return { success: false, error };
+  }
+};
+
